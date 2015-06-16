e082e6bd207789b43bb4837888ba3cfd0aa4f6a8
Youfar <wakafar@gmail.com>
2015-06-16 13:48:33 +0900

    RandomGraph 邻接矩阵应该能运行，怎么调用存在问题

--- src/MyMatrixGraph.java
+++ src/MyMatrixGraph.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by youfar on 15/6/15.
@@ -30,6 +31,58 @@ public class MyMatrixGraph implements MyGraph{
 
     public void addVertex(Object vertex){
         vertexList.add(vertexList.size(),vertex);
+    }
+
+    //判断两点有没有边，有返回假，没有返回真
+    public boolean isEdge(int v1, int v2){
+        if (edges[v1][v2] == 1 | edges[v2][v1] == 1)
+            return false;
+        else
+            return true;
+    }
+
+    //判断顶点在不在图里，在返回假，不在返回真
+    public boolean isVertex(Object vertex){
+        if (vertexList.contains(vertex)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public MyMatrixGraph matrixrandom(int V, int E) {
+        if(E > (long) V*(V-1)/2)
+            throw new IllegalArgumentException("too many edges");
+        if(E < 0)
+            throw new IllegalArgumentException("too few edges");
+        Random random = new Random();
+
+        MyMatrixGraph matrixRandomGraph = new MyMatrixGraph(V);
+        while(matrixRandomGraph.getNumOfEdges() < E){
+            int v = random.nextInt(V);
+            int w = random.nextInt(V);
+            boolean b1 = matrixRandomGraph.isEdge(v, w);
+            boolean b2 = matrixRandomGraph.isVertex(v);
+            boolean b3 = matrixRandomGraph.isVertex(w);
+
+            // v w不相等，图里都没有，也没有边
+            if((v != w) && b1 && b2 & b3){
+                matrixRandomGraph.addVertex(v);
+                matrixRandomGraph.addVertex(w);
+                matrixRandomGraph.addEdge(v, w);
+            }
+            //v w不相等，没有边，图里有w
+            if((v != w) && b1 && b2){
+                matrixRandomGraph.addVertex(v);
+                matrixRandomGraph.addEdge(v, w);
+            }
+            if((v != w) && b1 && b3){
+                matrixRandomGraph.addVertex(v);
+                matrixRandomGraph.addEdge(v, w);
+            }
+        }
+        return matrixRandomGraph;
+
     }
 
 
--- src/RandomGraph.java
+++ src/RandomGraph.java
@@ -3,7 +3,7 @@
  */
 import java.util.Random;
 public class RandomGraph {
-    public MyListGraph listrandom(int V, int E) {
+    /*public MyListGraph listrandom(int V, int E) {
         if(E > (long) V*(V-1)/2)
             throw new IllegalArgumentException("too many edges");
         if(E < 0)
@@ -32,9 +32,9 @@ public class RandomGraph {
         }
         return listRandomGraph;
 
-    }
+    }*/
 
-    public MyMatrixGraph matrixrandom(int V, int E) {
+    /*public MyMatrixGraph matrixrandom(int V, int E) {
         if(E > (long) V*(V-1)/2)
             throw new IllegalArgumentException("too many edges");
         if(E < 0)
@@ -42,6 +42,40 @@ public class RandomGraph {
         Random random = new Random();
 
         MyMatrixGraph matrixRandomGraph = new MyMatrixGraph(V);
+        while(matrixRandomGraph.getNumOfEdges() < E){
+            int v = random.nextInt(V);
+            int w = random.nextInt(V);
+            boolean b1 = matrixRandomGraph.isEdge(v, w);
+            boolean b2 = matrixRandomGraph.isVertex(v);
+            boolean b3 = matrixRandomGraph.isVertex(w);
+
+            // v w不相等，图里都没有，也没有边
+            if((v != w) && b1 && b2 & b3){
+                matrixRandomGraph.addVertex(v);
+                matrixRandomGraph.addVertex(w);
+                matrixRandomGraph.addEdge(v, w);
+            }
+            //v w不相等，没有边，图里有w
+            if((v != w) && b1 && b2){
+                matrixRandomGraph.addVertex(v);
+                matrixRandomGraph.addEdge(v, w);
+            }
+            if((v != w) && b1 && b3){
+                matrixRandomGraph.addVertex(v);
+                matrixRandomGraph.addEdge(v, w);
+            }
+        }
+        return matrixRandomGraph;
+
+    }*/
+
+    public static void main(String[] args) {
+        int V = 10;
+        int E = 10;
+
+        MyMatrixGraph matrixRandom0 = new MyMatrixGraph(V);
+        MyMatrixGraph matrixRandom1 = matrixRandom0.matrixRandom(V,E);
+
 
     }
 
